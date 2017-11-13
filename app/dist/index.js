83069d0bc4af8bf959b73d18220ebf366d626155
Itan Ortega <io@MacBook-Air-de-Itan.local>
2017-11-13 09:44:39 -0500

    primer componente

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,14 @@
-require('./modulo1')
-require('./modulo2')
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component{
+  render() {
+    return(
+      <h1>Hola itan o</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app)
